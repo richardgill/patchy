b2baf42a4c2b85727f81ab20bbc97c1e100c2322
Richard Gill <richard@rgill.co.uk>
2025-08-02 11:12:03 +0100

    wip

--- src/config/types.ts
+++ src/config/types.ts
@@ -40,3 +40,48 @@ export type CamelCaseResolvedConfig = {
 
 export type PartialResolvedConfig = Partial<ResolvedConfig>;
 export type PartialCamelCaseResolvedConfig = Partial<CamelCaseResolvedConfig>;
+
+export const CONFIG_FIELD_METADATA = {
+  repo_url: {
+    flag: "repo-url",
+    name: "Repository URL",
+    example: "https://github.com/user/repo.git",
+  },
+  repo_dir: {
+    flag: "repo-dir",
+    name: "Repository directory",
+    example: "./repo",
+  },
+  repo_base_dir: {
+    flag: "repo-base-dir",
+    name: "Repository base directory",
+    example: "./upstream",
+  },
+  patches_dir: {
+    flag: "patches-dir",
+    name: "Patches directory",
+    example: "./patches",
+  },
+  ref: {
+    flag: "ref",
+    name: "Git reference",
+    example: "main",
+  },
+  verbose: {
+    flag: "verbose",
+    name: "Verbose output",
+    example: "true",
+  },
+  dry_run: {
+    flag: "dry-run",
+    name: "Dry run mode",
+    example: "true",
+  },
+} as const satisfies Record<
+  keyof ResolvedConfig,
+  {
+    flag: keyof SharedFlags;
+    name: string;
+    example: string;
+  }
+>;
