6475058e517856a4671bfb460e16ebaf76066b47
Richard Gill <richard@rgill.co.uk>
2025-12-08 18:44:28 +0000

    Allow configuring fuzzFactor for patch application

--- src/commands/apply/apply-diff.ts
+++ src/commands/apply/apply-diff.ts
@@ -3,8 +3,9 @@ import { applyPatch } from "diff";
 export const applyDiff = (
   originalContent: string,
   diffContent: string,
+  fuzzFactor: number,
 ): string => {
-  const result = applyPatch(originalContent, diffContent, { fuzzFactor: 2 });
+  const result = applyPatch(originalContent, diffContent, { fuzzFactor });
 
   if (result === false) {
     throw new Error("Patch failed to apply - context does not match");
