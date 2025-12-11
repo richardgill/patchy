cce9d7cfb68bef44dbef54263e1e051e60bf3fdc
Richard Gill <richard@rgill.co.uk>
2025-12-11 17:51:10 +0000

    Colocate tests with source modules using blah.test.ts pattern

--- scripts/generate-schema.test.ts
+++ scripts/generate-schema.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "bun:test";
-import { generateJsonSchema } from "../../scripts/generate-schema";
+import { generateJsonSchema } from "./generate-schema";
 
 describe("generateJsonSchema", () => {
   it("should generate expected JSON schema", () => {
