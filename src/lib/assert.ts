50a812fb11370f97f090b88bad9801734105dbbd
Richard Gill <richard@rgill.co.uk>
2025-12-12 19:15:18 +0000

    Add test setup to disable ANSI color codes

--- src/lib/assert.ts
+++ src/lib/assert.ts
@@ -1,3 +1,4 @@
+// can we eliminate this?
 export const assertDefined = <T>(value: T | undefined, name: string): T => {
   if (value === undefined) {
     throw new Error(`Expected ${name} to be defined`);
